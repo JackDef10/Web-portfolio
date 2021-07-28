fedf77c41d611a8c9a90478f692235f63b2600a5
Jack D <Jackdefroand@gmail.com>
2021-07-28 11:21:08 +0100

    updated navigation bar functionality with javascript and created a animation when scrolling down the page. One issue when designing the nav bar is i was unable to scale the text on the nav bar with the rest of the nav bar. I will find a solution to this in the future with more experience

--- Resources/Javascript/Navbar.js
+++ Resources/Javascript/Navbar.js
@@ -1,24 +1,13 @@
-    "use strict";
+window.onscroll = function() {scrollFunction()};
 
-var indicator = document.querySelector('.nav-indicator');
-var items = document.querySelectorAll('.nav-item');
-
-function handleIndicator(el) {
-  items.forEach(function (item) {
-    item.classList.remove('is-active');
-    item.removeAttribute('style');
-  });
-  indicator.style.width = "".concat(el.offsetWidth, "px");
-  indicator.style.left = "".concat(el.offsetLeft, "px");
-  indicator.style.backgroundColor = el.getAttribute('active-color');
-  el.classList.add('is-active');
-  el.style.color = el.getAttribute('active-color');
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.querySelector(".nav").style.padding = "0px 20px";
+    document.getElementById("nav-text").style.fontSize = "1.5em";
+    document.querySelector("nav").style.left = "69%";
+  } else {
+    document.querySelector(".nav").style.padding = "20px 90px";
+    document.getElementById("nav-text").style.fontSize = "1.5em";
+    document.querySelector("nav").style.left = "32%";
+  }
 }
-
-items.forEach(function (item, index) {
-  item.addEventListener('click', function (e) {
-    handleIndicator(e.target);
-  });
-  item.classList.contains('is-active') && handleIndicator(item);
-});
-     
