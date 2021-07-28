b3f8c6c75da64f82d4356f91411e32ef6c722670
Jack D <Jackdefroand@gmail.com>
2021-07-28 12:55:17 +0100

    small javascript nav bar changes

--- Resources/Javascript/Navbar.js
+++ Resources/Javascript/Navbar.js
@@ -4,10 +4,10 @@ function scrollFunction() {
   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
     document.querySelector(".nav").style.padding = "0px 20px";
     document.getElementById("nav-text").style.fontSize = "1.5em";
-    document.querySelector("nav").style.left = "69%";
+    document.querySelector(".nav").style.left = "69%";
   } else {
     document.querySelector(".nav").style.padding = "20px 90px";
     document.getElementById("nav-text").style.fontSize = "1.5em";
-    document.querySelector("nav").style.left = "32%";
+    document.querySelector(".nav").style.left = "32%";
   }
 }
